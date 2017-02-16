5468f48bb162f9ad6574685bef22f2296c7dc439
david <david@akademia.co.jp>
2017-02-16 13:35:30 +0900

    wss

--- site/js/redux/actions.js
+++ site/js/redux/actions.js
@@ -22,7 +22,7 @@ export const init = asyncAction(() => {
 
 export const doSocketConnect = asyncAction(() => {
   // if(!webSocket)
-    webSocket = new WebSocket('ws://' + location.host)
+    webSocket = new WebSocket((location.protocol == 'https:'?'wss:':'ws:') + '//' + location.host)
 
   webSocket.onopen = (event) => {
     numConnectFails = 0
